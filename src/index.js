c90bb61755ef4d84386368351f12dadabe5c27d6
Jeronimo Santa Maria I <jerosantamariai@gmail.com>
2020-03-13 17:46:04 -0300

    almost finished

--- src/index.js
+++ src/index.js
@@ -14,16 +14,75 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 
+class TrafficLight extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            onred: 0,
+            onyellow: 0,
+            ongreen: 0,
 
-function TrafficLight(props){
-    return(
-            <div className="container">
-                <div className="redlight" id="red"></div>
-                <div className="yellowlight" id="yellow"></div>
-                <div className="greenlight" id="green"></div>
-            </div>
-            )
+        }
     }
- 
+
+    changeLight = (e) => {
+        if (e.target.id == "red" && this.state.onred === 0) {
+            e.target.classList.remove("alert-danger");
+            e.target.classList.add("bg-danger")
+            this.setState({
+                onred: 1
+            })
+         } else if (
+            e.target.id == "red" && this.state.onred === 1){
+            e.target.classList.add("alert-danger");
+            e.target.classList.remove("bg-danger")
+            this.setState({
+                onred: 0
+            })
+            }
+
+        if (e.target.id == "yellow" && this.state.onyellow === 0) {
+            e.target.classList.remove("alert-warning");
+            e.target.classList.add("bg-warning")
+            this.setState({
+                onyellow: 1
+            })
+        } else if (e.target.id == "yellow" && this.state.onyellow === 1){
+            e.target.classList.add("alert-warning");
+            e.target.classList.remove("bg-warning")
+            this.setState({
+                onyellow: 0
+            })
+        }
+
+        if (e.target.id == "green" && this.state.ongreen === 0) {
+            e.target.classList.remove("alert-success");
+            e.target.classList.add("bg-success")
+            this.setState({
+                ongreen: 1
+            })
+        } else if (e.target.id == "green" && this.state.ongreen === 1){
+            e.target.classList.add("alert-success");
+            e.target.classList.remove("bg-success")
+            this.setState({
+                ongreen: 0
+            })
+        }
+
+
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <div className={" alert-danger redLight"}  onClick={this.changeLight} id="red"></div>
+                <div className={" alert-warning yellowLight"} onClick={this.changeLight} id="yellow"></div>
+                <div className={" alert-success greenLight"} onClick={this.changeLight} id="green"></div>
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render(<TrafficLight />, document.querySelector("#root"));
